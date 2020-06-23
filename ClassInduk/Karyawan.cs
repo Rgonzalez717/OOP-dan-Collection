4cfa8dcf4df47acc0affe2c512593cce1f54e573
Rgonzalez717 <61915917+Rgonzalez717@users.noreply.github.com>
2020-06-23 12:59:40 +0700

    class induk karyawan

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_dan_collection.ClassInduk
+{
+    abstract class Karyawan
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+
+        public abstract int Gaji();
+
+    }
+}
